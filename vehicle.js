9cb41c94f98baaa1e65ae22d6dec182ecd8e9461
jamalsk9692 <139873056+jamalsk9692@users.noreply.github.com>
2025-09-30 20:47:47 +0530

    Update and rename api/vehicle.php to vehicle.js

---
+++ vehicle.js
@@ -0,0 +1,29 @@
+export default async function handler(req, res) {
+  res.setHeader("Content-Type", "application/json");
+
+  const { vehicleNumber } = req.query;
+
+  if (!vehicleNumber) {
+    return res.status(400).json({ error: "Missing vehicleNumber parameter" });
+  }
+
+  const apiUrl = `http://67.205.160.206:5000/api/vehicle/searchvehicle?vehicleNumber=${encodeURIComponent(vehicleNumber)}`;
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        "Host": "67.205.160.206:5000",
+        "User-Agent": "Node.js",
+        "Content-Type": "application/json",
+        "authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OGQ5MGNlZDUxZjk4MTBlNjEyOGM3Y2UiLCJ1c2VyVHlwZSI6IlVzZXIiLCJpYXQiOjE3NTkwNTUwODUsImV4cCI6MTc2NDIzOTA4NX0.uJV1jbFydJUtxBgH4B1yw73Zj2f520Xav85YJRpIMoY"
+      }
+    });
+
+    const data = await response.json();
+    return res.status(200).json(data);
+
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+}
